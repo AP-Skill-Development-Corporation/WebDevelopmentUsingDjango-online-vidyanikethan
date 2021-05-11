bc95b01f85e58ba5463d8f89e655e37789d83350
srikanya659qwe <51402676+srikanya659qwe@users.noreply.github.com>
2021-05-11 00:00:02 -0700

    done

---
+++ Day-1(OOPS)/sample.py
@@ -0,0 +1,18 @@
+class Calsi:
+    def add(n1,n2):
+        return n1+n2
+    def sub(n1,n2):
+        return n1-n2
+
+class Math:
+    def power(bval,exval):
+        return(bval**exval)
+    def isEven(val):
+        if val%2==0:
+            return True
+        return False
+    
+def isOdd(val):
+    if val%2!=0:
+        return True
+    return False
