02f731995a2901bf009d17981824b8f15aa1bbf0
SireeshaReyyi <sireeshareyyi@gmail.com>
2021-05-20 12:07:39 +0530

    Mail sending, file upload and mysql

--- Mailsending/Mailsending/settings.py
+++ Mailsending/Mailsending/settings.py
@@ -126,5 +126,5 @@ STATIC_URL = '/static/'
 EMAIL_USE_TLS  = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sireeshareyyi@gmail.com'
-EMAIL_HOST_PASSWORD = 'siri@gmail'
+EMAIL_HOST_USER = 'username'
+EMAIL_HOST_PASSWORD = 'password'
